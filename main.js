fb380e4f554dc914fce06cf16590f6a66931ca1a
Bartek Wieckowski <bart.it.pro@gmail.com>
2021-06-04 19:30:05 +0200

    adding function scrollActive to show sections

--- main.js
+++ main.js
@@ -29,6 +29,30 @@ const scrollHeader = function () {
 };
 window.addEventListener("scroll", scrollHeader);
 
+// Active link in menu
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+
 // MODAL
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
